81d7505f08e5a2550be92c66fa0fdadeb49975d4
Bill <clintoncsb@gmail.com>
2023-12-05 23:12:21 -0300

    🚨 fix: linter issues

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -2,7 +2,7 @@ abstract class Race {
   private _name: string;
   private _dexterity: number;
 
-  constructor (name: string, dexterity: number) {
+  constructor(name: string, dexterity: number) {
     this._name = name;
     this._dexterity = dexterity;
   }
@@ -17,10 +17,9 @@ abstract class Race {
 
   static createdRacesInstances(): number {
     throw new Error('Not implemented');
-  };
+  }
 
   abstract get maxLifePoints(): number;
-
 }
 
 export default Race;
